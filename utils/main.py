f1148a5aabf4fc0b61498ca2d1d8060e158f6b94
silverfish000 <245870771+silverfish000@users.noreply.github.com>
2025-12-22 22:52:58 +0100

    Ajout de la partie DB / Ajout des consequences des choix du user

--- utils/main.py
+++ utils/main.py
@@ -1,10 +1,18 @@
 # Ajouter/Modifier/Supprimer des mots de passe
 import os
 
+
+# ------------ db de categories et tout les mots de passes (non chiffres) ------------
+dictionnaire_test = {
+    'categories_mot_de_passe' : ""
+}
+# ------------ FIN ------------
+
+
 def clear() :
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def admin_choix() :
+def admin_choix() : # pour plus tard
     clear()
     print("-="*35)
     print("GESTIONNAIRE DES CHOIX UTILISATEURS (ONLY ADMIN)".center(65))
@@ -21,22 +29,32 @@ def aff_menu() :
     print("1) Ajouter un mot de passe\n")
     print("2) Modifier un mot de passe\n")
     print("3) Supprimer un mot de passe\n")
-    
-    
-
-
-def add_mdp() :
-    aff_menu()
-    choix_user = 0
     while (True) :
         try :
-            choix_user = int(input("Fais ton choix :"))
+            choix_user = int(input("Fais ton choix (QUE DES CHIFFRES) :"))
             break
         except ValueError :
             print("ERREUR : Tu ne dois que choisir des nombres")
-    
-add_mdp()
+    if (choix_user == 1) :
+        print("Tu as choisis l'option 1")
+    elif (choix_user == 2) :
+        print("Tu as choisis l'option 2")
+    else :
+        print("Tu as choisis l'option 3")
+    return choix_user
 
+
+
+def add_mdp(choix_user, categorie) :
+    if (choix_user == 1) :
+        input("Choisis")
+    elif (choix_user == 2) :
+        print("Tu as choisis l'option 2")
+    else :
+        print("Tu as choisis l'option 3")
+choix = aff_menu()
+
+add_mdp(choix, dictionnaire_test)
 
 
 # _0_
