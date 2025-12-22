6cdd6e82bb2946452c30bc6d36704416ef099364
f-Squash <121348298+f-Squash@users.noreply.github.com>
2025-12-23 00:47:16 +0100

    Update verif_mdp.py

--- utils/verif_mdp.py
+++ utils/verif_mdp.py
@@ -3,19 +3,63 @@ import string
 def verif_mdp(mot_de_passe, dictionnaire):
     
     if len(mot_de_passe) < 8:
-        return False, "Le mot de passe doit contenir au moins 8 caractere"
+        return False, "Le mot de passe doit contenir au moins 8 caracteres"
     
-    if not any(c.isupper() for c in mot_de_passe):
+
+    majuscule_trouve = False
+
+    for lettre in mot_de_passe:
+        if lettre.isupper():
+            majuscule_trouve = True
+            break
+
+    if not majuscule_trouve:
         return False, "Doit contenir au moins une majuscule"
     
-    if not any(c.islower() for c in mot_de_passe):
+
+    minuscule_trouve = False
+
+    for lettre in mot_de_passe:
+        if lettre.islower():
+            minuscule_trouve = True
+            break
+
+    if not minuscule_trouve:
         return False, "Doit contenir au moins une minuscule"
-    
-    if not any(c in string.punctuation for c in mot_de_passe):
+
+
+    caracteres_speciaux = "!@#$%^&*()_+-=?/"
+    special_trouve = False
+
+    for caractere in mot_de_passe:
+        if caractere in caracteres_speciaux:
+            special_trouve = True
+            break
+        
+    if not special_trouve:
         return False, "Doit contenir au moins un caractere special"
     
-    for cle, valeur in dictionnaire.items():
-        if cle != 'categories_mot_de_passe' and valeur == mot_de_passe:
-            return False, "Ce mot de passe existe dejaa"
+
+    for cle in dictionnaire:
+        if cle != 'categories_mot_de_passe':
+            if dictionnaire[cle] == mot_de_passe:
+                return False, "Ce mot de passe existe deja"
     
-    return True, "Mot de passe valide"
+    return True, "Mot de passe valider"
+
+
+def demander_mdp_valide(categorie_nom, dictionnaire):
+    while True:
+        nouveau_mdp = input(f"Entre le mot de passe pour {categorie_nom} : ")
+        valide, message = verif_mdp(nouveau_mdp, dictionnaire)
+        
+        if valide:
+            print()
+            print(f" {message}")
+            return nouveau_mdp
+        else:
+            print()
+            print(f" Erreur : {message}")
+            print()
+            print("Recommence !")
+            print()
