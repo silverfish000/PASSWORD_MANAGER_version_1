7aaa1712aa069cc1c735d9b6f83b6b63465e8c5f
silverfish000 <245870771+silverfish000@users.noreply.github.com>
2025-12-26 20:44:03 +0100

    Gros patch general avec pleins de nouveautes !

--- utils/config.py
+++ utils/config.py
@@ -15,21 +15,6 @@ dictionnaire_auto = {
     ]
 }
 
-'''dictionnaire_auto_add =[
-        ("Netflix", ""),
-        ("Amazon", ""),
-        ("Github", ""),
-        ("Apple", ""),
-        ("Google", ""),
-        ("Facebook", ""),
-        ("Spotify", ""),
-        ("Steam", ""),
-        ("Adobe", ""),
-        ("LinkedIn", ""),
-        ("Instagram", ""),
-        ("Discord", "")
-    ]'''
-
 
 dictionnaire_auto_add=[
     {
@@ -37,18 +22,73 @@ dictionnaire_auto_add=[
         'created_at' : '2025-12-24',
         'mdp' : ''
     },
-    {
-        
+{
+        'name' : 'Amazon',
+        'created_at' : '2025-12-24',
+        'mdp' : ''
+    },
+{
+        'name' : 'Github',
+        'created_at' : '2025-12-24',
+        'mdp' : ''
+    },
+{
+        'name' : 'Apple',
+        'created_at' : '2025-12-24',
+        'mdp' : ''
+    },
+{
+        'name' : 'Google',
+        'created_at' : '2025-12-24',
+        'mdp' : ''
+    },
+{
+        'name' : 'Facebook',
+        'created_at' : '2025-12-24',
+        'mdp' : ''
+    },
+{
+        'name' : 'Spotify',
+        'created_at' : '2025-12-24',
+        'mdp' : ''
+    },
+{
+        'name' : 'Steam',
+        'created_at' : '2025-12-24',
+        'mdp' : ''
+    },
+{
+        'name' : 'Adobe',
+        'created_at' : '2025-12-24',
+        'mdp' : ''
+    },
+{
+        'name' : 'LinkedIn',
+        'created_at' : '2025-12-24',
+        'mdp' : ''
+    },
+{
+        'name' : 'Instagram',
+        'created_at' : '2025-12-24',
+        'mdp' : ''
+    },
+{
+        'name' : 'Discord',
+        'created_at' : '2025-12-24',
+        'mdp' : ''
     }
 ]
-print(dictionnaire_auto_add[0]['mdp'])
 
-
-dictionnaire_interdit = {
+dictionnaire_interdit = { # dictionnaire interdit qui sert a rien on va pas se mentir
     'categories_mots_interdits' : [
         "", "admin", "system", "root", "config",
         "settings", "test", "temp", "default",
-        "clear", "Silver"
+        "clear", "Silver", "Password", "12345678", "qwerty",
+        "letmein", "welcome", "monkey", "abc123", "football", "iloveyou", "admin123",
+        "password1", "12345", "123456789", "1234567890", "password!", "Passw0rd", "Admin@123",
+        "Qwerty123", "Welcome1", "LetMeIn!", "Monkey@1", "Football!", "ILoveYou1",
+        "1234abcd", "abcd1234", "Temp@1234", "Default1!", "Clear@2024",
+        "Silver2024!", "Password2024#", "Admin2024$"
     ]
 }
 
--- utils/main.py
+++ utils/main.py
@@ -5,28 +5,34 @@ import time
 from verif_mdp import verif_mdp, demander_mdp_valide
 from config import dictionnaire_auto, dictionnaire_auto_add, dictionnaire_interdit
 
-def clear() :
+
+def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def privileges_admin() :
+    global privileges_admin
+    privileges_admin = False
+    if (privileges_admin) :
+        return True
 
-
-def code_admin_verif() :
+def code_admin_verif():
     clear()
     print("VERIFICATION CODE ADMIN".center(70, "="))
     essai = 3
     code_admin_input = input("Entre le code ADMIN : ")
-    if (code_admin_input == os.getenv("APP.PASSWORD")) :
+    if (code_admin_input == os.getenv("APP.PASSWORD")):
         print("Code ADMIN valide tu vas etre rediriger vers le menu ADMIN")
         time.sleep(2)
         return True
-    else :
-        while (essai > 1) :
+    else:
+        while (essai > 1):
             essai -= 1
             clear()
             print("VERIFICATION CODE ADMIN".center(70, "="))
             code_admin_input = input("ERREUR : Code ADMIN invalide, re-essaye : ")
-            if (code_admin_input == os.getenv("APP.PASSWORD")) :
+            if (code_admin_input == os.getenv("APP.PASSWORD")):
                 print("Code ADMIN valide tu vas etre rediriger vers le menu ADMIN")
+                privileges_admin()
                 time.sleep(2)
                 return True
         clear()
@@ -36,9 +42,9 @@ def code_admin_verif() :
         return False
 
 
-def tutoriel() :
+def tutoriel():
     clear()
-    print("TUTORIEL PASSWORLD".center(70, "=")) # Tutoriel interactif
+    print("TUTORIEL PASSWORLD".center(70, "="))
     print("\nBienvenue dans le tutoriel de PASSWORLD !\n")
     print("Ici, tu apprendras a utiliser les fonctionnalites principales de l'application.\n")
     time.sleep(1.5)
@@ -64,9 +70,106 @@ def tutoriel() :
     print("Bonne gestion de tes mots de passe avec PASSWORLD !")
     time.sleep(2)
     input("Appuie sur Entrée pour continuer...")
-tutoriel()
 
-def bonjour() :
+def admin_choix(pseudo):
+    global dictionnaire_auto
+    global dictionnaire_auto_add
+    global choix_admin_categories
+
+    clear()
+    while (True):
+        print("="*70)
+        print(" CONFIGURATION ADMIN ".center(70, "═"))
+        print(f"connecte en tant que {pseudo}".center(70))
+        print("="*70)
+        noms = [tuples['name'] for tuples in dictionnaire_auto_add]
+        print(f"\n Catégories actuelles : {', '.join(noms)}")
+        print("-" * 30)
+        print("- Tapez le NOM d'une catégorie pour l'ajouter")
+        print("- Tapez 'AUTO' pour charger la configuration par défaut")
+        print("- Tapez 'STOP' pour finaliser et quitter")
+        print("-" * 30)
+        choix_admin_categories = input("Ecris ton choix -->")
+        mdp = ''
+        clear()
+        if (choix_admin_categories.lower() == 'auto'):
+            choix_user_auto = input("ATTENTION TA LISTE QUE TU AS CREER VA ETRE SUPPRIMEE TU ES SUR QUE TU VEUX CONTINUER ? (o/n)")
+            if (choix_user_auto.lower() == 'o'):
+                if (dictionnaire_auto == dictionnaire_auto_add):
+                    print("Le systeme 'auto' est deja activer sur ta machine")
+                    time.sleep(2)
+                else:
+                    print("Tu as choisis la fonction 'auto' le systeme va le mettre en place")
+                    dictionnaire_auto_add = dictionnaire_auto
+                    time.sleep(2)
+            elif (choix_user_auto.lower() == 'n'):
+                clear()
+            else:
+                print("ERREUR : ONLY (o/n)")
+        elif (choix_admin_categories.lower() == 'stop'):
+            print("tu viens de taper 'STOP' tu vas etre rediriger vers le menu dans quelques secondes ...")
+            time.sleep(2)
+            break
+        else:
+            noms_existants = [tuples['name'] for tuples in dictionnaire_auto_add]
+            if (choix_admin_categories.lower() in dictionnaire_interdit['categories_mots_interdits']):
+                print(f"ATTENTION : '{choix_admin_categories}' est un mot interdit")
+                time.sleep(3)
+                clear()
+            elif (choix_admin_categories in noms_existants):
+                clear()
+                print(f"🔴 La categorie '{choix_admin_categories}' existe deja\n")
+                time.sleep(2)
+            else:
+                dictionnaire_auto_add['categories_mot_de_passe'].append((choix_admin_categories, mdp))
+                clear()
+                print(f"✅ La categorie '{choix_admin_categories}' a bien etait rajoutee !\n")
+                time.sleep(2)
+
+
+def menu_admin(pseudo):
+    while (True):
+        clear()
+        print("="*70)
+        print(" MENU ADMIN ".center(70, "═"))
+        print(f"connecte en tant que {pseudo}".center(70))
+        print("="*70)
+        print("1) Configurer les categories de mot de passe\n")
+        print("2) Voir les logs de la session\n")
+        print("3) Retour au menu principal\n")
+        try:
+            choix_user_admin = int(input("Fais ton choix (QUE DES CHIFFRES) :"))
+        except ValueError:
+            clear()
+            print("ERREUR : ONLY (1 / 2 / 3)")
+            time.sleep(2)
+            continue
+        if (choix_user_admin == 1):
+            admin_choix(pseudo)
+        elif (choix_user_admin == 2):
+            clear()
+            print("VOIR LES LOGS DE LA SESSION".center(70, "-"))
+            logs_file = "admin_attempts.log"
+            try:
+                with open(logs_file, "r") as file:
+                    logs = file.readlines()
+                    if logs:
+                        print("\n".join(logs))
+                    else:
+                        print("Aucun log disponible.")
+            except FileNotFoundError:
+                print("Aucun log disponible.")
+            input("Appuie sur Entrée pour continuer...")
+        elif (choix_user_admin == 3):
+            clear()
+            break
+        else:
+            clear()
+            print("ERREUR : ONLY (1 / 2 / 3)")
+            time.sleep(2)
+
+
+def bonjour():
     clear()
     print("-="*40)
     print(r"""
@@ -77,132 +180,79 @@ def bonjour() :
 ██║     ██║  ██║███████║███████║    ╚███╔███╔╝╚██████╔╝██║  ██║███████╗██████╔╝
 ╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝     ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═════╝ 
 """)
-
     print("by Silver".center(72))
     print("-="*40)
-    while (True) :
+    while (True):
         pseudo = input("Quel est ton pseudo ? ")
-        if (len(pseudo) < 3 or len(pseudo) > 15) :
+        if (len(pseudo) < 3 or len(pseudo) > 15):
             clear()
             print("ERREUR : Ton pseudo doit contenir entre 3 et 15 caracteres")
             time.sleep(2)
-        elif (not pseudo.isalnum()) :
+        elif (not pseudo.isalnum()):
             clear()
             print("ERREUR : Ton pseudo ne doit contenir que des caracteres alphanumeriques (lettres et chiffres)")
             time.sleep(2)
-        elif (pseudo.lower() in dictionnaire_interdit['categories_mots_interdits']) :
+        elif (pseudo.lower() in dictionnaire_interdit['categories_mots_interdits']):
             clear()
             print(f"ERREUR : Le pseudo '{pseudo}' est interdit, choisis-en un autre")
             time.sleep(2)
-        else :
+        else:
             print(f"Bienvenue {pseudo} ! Tu vas etre rediriger vers le menu principal dans quelques secondes ...")
             time.sleep(2)
             break
     clear()
-    while (True) :
+    while (True):
         code_admin = input("As-tu un code ADMIN ou un code de triche ? (o/n)")
-        if (code_admin.lower() == 'o') :
+        if (code_admin.lower() == 'o'):
             clear()
-            if (code_admin_verif()) :
+            if (code_admin_verif()):
                 print("Code de triche active")
-                menu_admin() # code de triche & admin panel
+                menu_admin(pseudo)
+                privileges_admin = True
                 time.sleep(1)
                 break
-        elif (code_admin.lower() == 'n') :
+        elif (code_admin.lower() == 'n'):
             clear()
             print("Code de triche desactive")
             time.sleep(1)
             break
-        else :
+        else:
             clear()
             print("ERREUR : (ONLY o/n)")
-    while (True) :
+    while (True):
         choix_user = input("Veux-tu faire le tutoriel ? (o/n)")
-        if (choix_user.lower() == 'o') :
+        if (choix_user.lower() == 'o'):
             clear()
             tutoriel()
             time.sleep(1)
             break
-        elif (choix_user.lower() == 'n') :
+        elif (choix_user.lower() == 'n'):
             clear()
             print("Tutoriel desactive")
             time.sleep(1)
             break
-        else :
+        else:
             clear()
             print("ERREUR : (ONLY o/n)")
     return pseudo
 
-pseudo = bonjour() # Bienvenue sur PASSWORLD
 
-
-
-
-def admin_choix(pseudo) :
-    global dictionnaire_auto
-    global dictionnaire_auto_add
-    global choix_admin_categories
-
-    clear()
-    while (True) :
-        print("="*70)
-        print(" CONFIGURATION ADMIN ".center(70, "═"))
-        print(f"connecte en tant que {pseudo}".center(70))
-        print("="*70)
-        noms = [caca[0] for caca in dictionnaire_auto_add]
-        print(f"\n Catégories actuelles : {', '.join(noms)}")
-        print("-" * 30)
-        print("- Tapez le NOM d'une catégorie pour l'ajouter")
-        print("- Tapez 'AUTO' pour charger la configuration par défaut")
-        print("- Tapez 'STOP' pour finaliser et quitter")
-        print("-" * 30)
-        choix_admin_categories = input("Ecris ton choix -->")
-        mdp = ''
-        clear()
-        if (choix_admin_categories.lower() == 'auto') :
-            choix_user_auto = input("ATTENTION TA LISTE QUE TU AS CREER VA ETRE SUPPRIMEE TU ES SUR QUE TU VEUX CONTINUER ? (o/n)")
-            if (choix_user_auto.lower() == 'o') :
-                if (dictionnaire_auto == dictionnaire_auto_add) :
-                    print("Le systeme 'auto' est deja activer sur ta machine")
-                    time.sleep(2)
-                else :
-                    print("Tu as choisis la fonction 'auto' le systeme va le mettre en place")
-                    dictionnaire_auto_add = dictionnaire_auto
-                    time.sleep(2)
-            elif (choix_user_auto.lower() == 'n') :
-                clear()
-            else :
-                print("ERREUR : ONLY (o/n)")
-        elif (choix_admin_categories.lower() == 'stop') :
-            print("tu viens de taper 'STOP' tu vas etre rediriger vers le menu dans quelques secondes ...")
-            time.sleep(2)
-            break
-        else :
-            noms_existants = [tuples[0] for tuples in dictionnaire_auto_add['categories_mot_de_passe']]
-            if (choix_admin_categories.lower() in dictionnaire_interdit['categories_mots_interdits']) :
-                print(f"ATTENTION : '{choix_admin_categories}' est un mot interdit")
-                time.sleep(3)
-                clear()
-            elif (choix_admin_categories in noms_existants):
-                clear()
-                print(f"🔴 La categorie '{choix_admin_categories}' existe deja\n")
-                time.sleep(2)
-            else :
-                dictionnaire_auto_add['categories_mot_de_passe'].append((choix_admin_categories, mdp))
-                clear()
-                print(f"✅ La categorie '{choix_admin_categories}' a bien etait rajoutee !\n")
-                time.sleep(2)
-
-def add_mdp(dictionnaire) :
-    while (True) :
+def add_mdp(dictionnaire):
+    while (True):
         clear()
         print("="*75)
         print(" AJOUT MOT DE PASSE ".center(75, "/"))
         print("="*75)
         print("1) AJOUTER UN MOT DE PASSE\n")
         print("2) QUITTER (Retour au menu)")
-        choix_user_add_mdp = int(input(""))
-        if (choix_user_add_mdp == 1) : # Ajouter un mot de passe
+        try:
+            choix_user_add_mdp = int(input(""))
+        except ValueError:
+            clear()
+            print("ERREUR : ONLY (1 / 2)")
+            time.sleep(2)
+            continue
+        if (choix_user_add_mdp == 1):
             clear()
             print("AJOUTER UN MOT DE PASSE".center(75, "-"))
             noms = [caca['name'] for caca in dictionnaire]
@@ -210,27 +260,27 @@ def add_mdp(dictionnaire) :
             print("-" * 30)
             categorie_nom = input("Dans quelle catégorie veux-tu ajouter un mot de passe ? ")
             categorie_trouve = False
-            for categorie in dictionnaire :
-                if categorie['name'].lower() == categorie_nom.lower() :
+            for categorie in dictionnaire:
+                if categorie['name'].lower() == categorie_nom.lower():
                     categorie_trouve = True
                     nouveau_mdp = demander_mdp_valide(categorie_nom, dictionnaire_auto)
                     categorie['mdp'] = nouveau_mdp
                     print(f"\n Le mot de passe pour la catégorie '{categorie_nom}' a été ajouté/modifié avec succès.")
                     time.sleep(2)
                     break
-            if not categorie_trouve :
+            if not categorie_trouve:
                 print(f"\n ERREUR : La catégorie '{categorie_nom}' n'existe pas.")
                 time.sleep(2)
-        elif (choix_user_add_mdp == 2) :
+        elif (choix_user_add_mdp == 2):
             clear()
             break
-        else :
+        else:
             print("ERREUR : (ONLY 1 / 2)")
             time.sleep(2)
-            
 
-def gerer_mdp(dictionnaire) :
-    while (True) :
+
+def gerer_mdp(dictionnaire):
+    while (True):
         clear()
         print("="*75)
         print(" GERER MOT DE PASSE ".center(75, "/"))
@@ -240,69 +290,85 @@ def gerer_mdp(dictionnaire) :
         print("3) RECHERCHER UN MOT DE PASSE (in build)\n")
         print("4) Afficher tous les mots de passe (in build)\n")
         print("5) QUITTER (Retour au menu)")
-        choix_user_gerer_mdp = int(input(""))
-        if (choix_user_gerer_mdp == 1) : # Ajouter un mot de passe
+        try:
+            choix_user_gerer_mdp = int(input(""))
+        except ValueError:
+            clear()
+            print("ERREUR : ONLY (1 / 2 / 3 / 4 / 5)")
+            time.sleep(2)
+            continue
+        if (choix_user_gerer_mdp == 1):
             clear()
             add_mdp(dictionnaire)
             input("Appuie sur Entrée pour continuer...")
-        elif (choix_user_gerer_mdp == 2) :
+        elif (choix_user_gerer_mdp == 2):
             clear()
             print("SUPPRIMER UN MOT DE PASSE".center(75, "-"))
-            # Ajouter le systeme supprimer_mdp
             input("Appuie sur Entrée pour continuer...")
-        elif (choix_user_gerer_mdp == 3) :
+        elif (choix_user_gerer_mdp == 3):
+            clear()
+            print("RECHERCHER UN MOT DE PASSE".center(75, "-"))
+            input("Appuie sur Entrée pour continuer...")
+        elif (choix_user_gerer_mdp == 4):
+            clear()
+            print("AFFICHER TOUS LES MOTS DE PASSE".center(75, "-"))
+            input("Appuie sur Entrée pour continuer...")
+        elif (choix_user_gerer_mdp == 5):
             clear()
             break
-        else :
-            print("ERREUR : (ONLY 1 / 2 / 3)")
+        else:
+            clear()
+            print("ERREUR : (ONLY 1 / 2 / 3 / 4 / 5)")
             time.sleep(2)
 
-def aff_menu() :
-    while (True) :
+
+def aff_menu(pseudo):
+    while (True):
         clear()
         print("-="*35)
-        print("GESTIONNAIRE DE MOT DE PASSE".center(65))
+        print("MENU PRINCIPAL".center(70))
+        print(f"Connecte en tant que : {pseudo}".center(70))
         print("-="*35)
-        print("by Silver\n")
-        print("1 - Ajouter un mot de passe (v1 pas fini)\n")
-        print("2 - Gerer les mots de passes (in build)\n")
-        print("4 - Paramètres (in build)\n")
-        print("5 - Tutoriel (in build)\n")
-        print("6 - Quitter\n")
-        try :
-            choix_user = int(input("Fais ton choix (QUE DES CHIFFRES) :"))
-            if (choix_user == 1) :
+        print("1 - Ajouter un mot de passe\n")
+        print("2 - Gerer les mots de passe\n")
+        print("3 - Option 3 (in build)\n")
+        print("4 - Option 4 (in build)\n")
+        print("5 - Quitter l'application\n")
+        try:
+            choix_user = int(input(f"{pseudo} fais ton choix (QUE DES CHIFFRES) :"))
+            if (choix_user == 1):
                 add_mdp(dictionnaire_auto_add)
-            elif (choix_user == 2) :
+            elif (choix_user == 2):
                 gerer_mdp(dictionnaire_auto_add)
-            elif (choix_user == 3) :
+            elif (choix_user == 3):
                 print("Tu as choisis l'option 3")
-                # Ajouter le systeme supp_mdp
-            elif (choix_user == 4) :
+            elif (choix_user == 4):
                 print("Tu as choisis l'option 4")
-                admin_choix(pseudo)
-            elif (choix_user == 5) :
-                while (True) :
+            elif (choix_user == 5):
+                while (True):
                     clear()
-                    decision_quitter = input("Vous etes sur de vouloir quitter ? (o/n)")
-                    if (decision_quitter.lower() == 'o') :
+                    decision_quitter = input(f"{pseudo} tu es sur de vouloir quitter ? (o/n)")
+                    if (decision_quitter.lower() == 'o'):
                         clear()
                         print("A la prochaine mon bro !!!!")
                         time.sleep(1.5)
                         exit()
-                    elif (decision_quitter.lower() == 'n') :
+                    elif (decision_quitter.lower() == 'n'):
                         break
-                    else :
+                    else:
                         print("ERREUR : ONLY (o/n)")
                         time.sleep(2)
-            else :
+            else:
                 clear()
-                print("ERREUR : ONLY (1 - 5)")
+                print("ERREUR : ONLY (1 / 2 / 3 / 4 / 5)")
                 time.sleep(2)
-        except ValueError :
-            print()
+        except ValueError:
+            clear()
+            print("ERREUR : ONLY (QUE DES CHIFFRES)")
+            time.sleep(2)
 
-aff_menu()
-
-
-# _0_
+if __name__ == "__main__":
+    pseudo = bonjour()
+    # init logs admin
+    admin_attempts_file = "admin_attempts.log"
+    aff_menu(pseudo)
